c6120c14dfa42f5353c6570a0ae5683cbf68fbf1
auriellove <auriel.e.love@gmail.com>
2022-05-19 21:09:35 -0400

    fixed loop

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ const coin = document.getElementById("typeOfCoin");
 const numberOfCoin = document.getElementById("numberOfCoins");
 
 button.addEventListener("click", () => {
-  const image = document.createElement("img");
+  
 
-  image.style.width = "72px";
+  for (let i = 0; i < numberOfCoin.value; i++) {
+    const image = document.createElement("img");
 
-  if (coin.value === "penny") {
-    image.src = "images/penny.png";
-  } else if (coin.value === "nickel") {
-    image.src = "images/nickel.png";
-  } else if (coin.value === "dime") {
-    image.src = "images/dime.png";
-  } else if (coin.value === "quarter") {
-    image.src = "images/quarter.png";
-  }
-
-  for (let i = 0; i <= numberOfCoin.value; i++) {
+    image.style.width = "72px";
+  
+    if (coin.value === "penny") {
+      image.src = "images/penny.png";
+    } else if (coin.value === "nickel") {
+      image.src = "images/nickel.png";
+    } else if (coin.value === "dime") {
+      image.src = "images/dime.png";
+    } else if (coin.value === "quarter") {
+      image.src = "images/quarter.png";
+    }
     console.log("loop run");
     document.querySelector("#coinDisplay").appendChild(image);
   }
