9205227a514ffb98dd36789d52523549dbfcc4ab
Briana Hunter <brianahunter@Brianas-MacBook-Pro.local>
2022-05-19 20:18:09 -0400

    Added images DOM

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const button= document.getElementById("button");
+const button = document.getElementById("button");
 button.addEventListener("click", () => {
-const image= document.createElement("img");
-img src="" alt=""
-
+  const image = document.createElement("img");
+  image.style.width = "72px";
+  image.src = "images/penny.png";
+  image.src = "images/nickel.png";
+  image.src = "images/dime.png";
+  image.src = "images/quarter.png";
+  document.querySelector("#coinDisplay").appendChild(image);
 });
